4490be9726caf6a4ccdb095ee297c3e5aea770eb
Chu-Junjie <junjie.chu@my.jcu.edu.au>
2025-11-09 22:14:32 +0800

    Delete Form directory

--- Form/signup-validation.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.signup-form').addEventListener('submit', function(e) {
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
-    
-    if (password !== confirmPassword) {
-        e.preventDefault();
-        alert('Passwords do not match!');
-    }
-});
